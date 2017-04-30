ce53ed4498381119182409d3fb4817ca375d73b8
canek <sknest@gmail.com>
2017-04-30 11:47:05 +0200

    Add POST todos

---
+++ playground/mongodb-update.js
@@ -0,0 +1,20 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=>{
+if(err){
+  return console.log('Unable to connect to MongoDB server');
+}
+console.log('Connected to MongoDB server');
+
+db.collection('Users').findOneAndUpdate({name: 'Jovana'}, {
+  $set: {name: 'Anastasija'}, $inc:{age:1}}, {returnOriginal: false}).then((result)=>{
+  console.log(result);
+});
+
+
+//db.close();
+});
